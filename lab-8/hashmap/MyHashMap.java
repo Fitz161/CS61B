2a804b7395f0485a95080ce7d789101abfde1604
Fitz161 <Fitz1611@outlook.com>
2023-01-09 18:32:45 +0800

    lab8 sp21 solution

--- lab-8/hashmap/MyHashMap.java
+++ lab-8/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -27,12 +27,25 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /* Instance Variables */
     private Collection<Node>[] buckets;
+
+    private Set<K> keys;
     // You should probably define some more!
+    private int initialSize = 16;
+    private double loadFactor = 0.75;
+
+    private int size;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        buckets = createTable(initialSize);
+        keys = new HashSet<>();
+        size = 0;
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this.initialSize = initialSize;
+        buckets = createTable(initialSize);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +54,17 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        this.initialSize = initialSize;
+        this.loadFactor = maxLoad;
+        buckets = createTable(initialSize);
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
     }
 
     /**
@@ -69,7 +86,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<>();
     }
 
     /**
@@ -82,10 +99,128 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
-        return null;
+        /*
+        * When creating a new Collection<Node>[] to store in our buckets variable,
+        * be aware that in Java, you cannot create an array of parameterized type.
+        * Collection<Node> is a parameterized type, because we parameterize the
+        * Collection class with the Node class. Therefore, the expression new
+        * Collection<Node>[size] is illegal, for any given size. To get around this,
+        * you should instead create a new Collection[size], where size is the desired size.
+        * The elements of a Collection[] can be a collection of any type, like a
+        * Collection<Integer> or a Collection<Node>. For our purposes, we will only add
+        * elements of type Collection<Node> to our Collection[].
+        */
+        Collection<Node>[] table = new Collection[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            table[i] = createBucket();
+        }
+        return table;
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+    @Override
+    public void clear() {
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i].clear();
+        }
+        keys.clear();
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (keys.contains(key)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        if (containsKey(key)) {
+            int hashCode = Math.floorMod(key.hashCode(), initialSize);
+            for (Node node : buckets[hashCode]) {
+                if (node == null) {
+                    continue;
+                } else if (node.key.equals(key)) {
+                    return node.value;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (containsKey(key)){
+            for (Collection<Node> bucket : buckets) {
+                for (Node node : bucket) {
+                    if (node == null) {
+                        continue;
+                    }
+                    else if (node.key == key){
+                        node.value = value;
+                        return;
+                    }
+                }
+            }
+        }
+        else {
+            double loadFactor = (size() + 1) / initialSize;
+            if (loadFactor > this.loadFactor) {
+                initialSize *= 2;
+                Collection<Node>[] newBuckets = createTable(initialSize);
+                for (Collection<Node> bucket : buckets) {
+                    for (Node node : bucket) {
+                        if (node == null) {
+                            continue;
+                        }
+                        int hashCode = Math.floorMod(node.key.hashCode(), initialSize);
+                        newBuckets[hashCode].add(node);
+                        buckets = newBuckets;
+                    }
+                }
+            }
+            int hashCode = Math.floorMod(key.hashCode(), initialSize);
+            buckets[hashCode].add(createNode(key, value));
+            keys.add(key);
+            size += 1;
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return keys;
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+//        return new Iterator<K>() {
+//            @Override
+//            public boolean hasNext();
+//
+//            @Override
+//            public K next();
+//        };
+        return keys.iterator();
+    }
 
 }
